0c6a418afa9094fd5e7b2f28afc34fa56706a6c6
Juan Gutiérrez <juanesguti19@gmail.com>
2020-10-03 15:28:49 -0500

    Primer commit

---
+++ src/java/com/udea/ejb/AccountFacade.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.ejb;
+
+import com.udea.modelo.Account;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author JuanGutierrez
+ */
+@Stateless
+public class AccountFacade extends AbstractFacade<Account> implements AccountFacadeLocal {
+
+    @PersistenceContext(unitName = "laboratorio1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    
+    
+    public AccountFacade() {
+        super(Account.class);
+    }
+
+    @Override
+    public boolean checkLogin(String u, String p) {
+        Query q = em.createQuery("select a from Account a where a.username=:u and a.password=:p");
+        q.setParameter("u", u);
+        q.setParameter("p", p);
+        return q.getResultList().size()>0;
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : 09-24-2020, 06:34:08 PM
+    Author     : JuanGutierrez
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Vista Principal</title>
+    </head>
+    <body>
+        <jsp:include page="menu.jsp"></jsp:include>
+        <h1>PÁGINA PARA GESTIÓN DE PERSONAL</h1>
+    </body>
+</html>
